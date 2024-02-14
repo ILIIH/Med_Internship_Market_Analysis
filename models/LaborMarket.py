ca3473126f1df41954852239487572506912b921
ILLIA <69596138+ILIIH@users.noreply.github.com>
2024-02-14 14:55:28 +0000

    Changed data scraping algorithm

--- models/LaborMarket.py
+++ models/LaborMarket.py
@@ -5,3 +5,10 @@ class LaborMarket:
     avg_salary: float 
     avg_additional_sal: float
     offers_amount: float
+
+@dataclass
+class JobEntity:
+    salary: String 
+    title: String
+    country: String
+    company_name: String
--- scraping/IE/collect_data.py
+++ scraping/IE/collect_data.py
@@ -1,29 +1,80 @@
-import scrapy
-from scrapy.crawler import CrawlerProcess
-from scrapy import Request
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
-
-class MySpider(scrapy.Spider):
-    name = 'myspider'
-    start_urls = ['https://www.jobs.ie/jobs/nurse/in-ireland?radius=20&searchOrigin=Resultlist_top-search']
-    
-    custom_settings = {
-    'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'DOWNLOAD_DELAY': 2,
-    'DEFAULT_REQUEST_HEADERS': {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Language': 'en',
-        'Referer': 'https://www.google.com/',
-      }
-    }
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-    def parse(self, response):
-        print(response.text)
-        
-        # You can continue your parsing logic here
 
-# Run the spider
-process = CrawlerProcess()
-process.crawl(MySpider)
-process.start()
+def preSetting():
+
+    try:
+        # Find and click the "Accept All" button
+        accept_all_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//div[@id="ccmgt_explicit_accept" and @class="privacy-prompt-button primary-button ccmgt_accept_button"]'))
+        )
+        accept_all_button.click()
+
+    except Exception as e:
+        print(f"Exception while clicking 'Accept All' button: {e}")
+        return None  # or any other value to indicate failure
+
+def getPageCount():
+    try:
+        # Find the specified <a> element
+        target_link = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//a[@class="res-9hatlb" and @data-genesis-element="BUTTON"]'))
+        )
+        aria_label_value = target_link.get_attribute("aria-label")
+        parts = aria_label_value.split(" of ")
+        n_value_str = parts[1]
+        return  int(n_value_str)
+
+    except Exception as e:
+        print(f"Exception while locating target link: {e}")
+        return None  
+
+def navigateToNextPage(i,max_value) :
+        next_page = f"{i} of {max_value}"
+        next_link = f'//a[@class="res-14ttpod" and @data-genesis-element="BUTTON" and @aria-label="{next_page}"]'
+
+        target_link = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,next_link ))
+        )
+
+        target_link.click()
+
+# Specify the URL of the website you want to scrape
+url = "https://www.jobs.ie/jobs/nurse/in-ireland?radius=20&searchOrigin=Resultlist_top-search"
+
+# Create a new instance of the Chrome driver
+driver = webdriver.Chrome()
+
+driver.get(url)
+
+preSetting()
+max_value = getPageCount()
+
+for i in range(1,max_value+1):
+    try:
+        if i != 1 : 
+            navigateToNextPage(i,max_value)
+
+        job_titles = driver.find_elements(By.XPATH, '//div[@class="res-nehv70" and @data-genesis-element="BASE"]')
+        job_salaries = driver.find_elements(By.XPATH, '//span[@class="res-1vpeoiv" and @data-at="job-item-salary-info"]')
+        print(f"{len(job_titles)} {len(job_salaries)}")
+
+        for i in range(0,len(job_titles)):
+            print(f"{job_titles[i].text} {job_salaries[i].text}")
+
+        time.sleep(2)  
+
+    except Exception as e:
+        print(f"Exception: {e}")
+        break
+
+
+# Close the browser
+driver.quit()
+
+
